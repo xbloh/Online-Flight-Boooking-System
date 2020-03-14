f97495e0a03affc351ba5f74ab4b8f5ccf0e7485
jiacheng-0 <michael-tjc@hotmail.com>
2020-03-14 17:14:20 +0800

    Update connection_manager.py

--- connection_manager.py
+++ connection_manager.py
@@ -1,22 +1,11 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from os import environ
+'''
+Created by Jia Cheng
+2020/03/14
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/book'
-# set dbURL=mysql+mysqlconnector://root@localhost:3306/book
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 1800}
+Purpose: 
+    - to store the connection string to our central AWS RDS database.
+    - any microservices related settings
 
-db = SQLAlchemy(app)
-CORS(app)
+'''
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
-    # if debug is True, The program refreshed the server if there are changes made to the code.
-
-    # Either turn this on, or turn autosave on
-    # If both on, the terminal will keep on refreshing
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://flight_admin:6kKVm7C2PHtVtgGT@esd-g7t6-rds.cs2kfkrucphj.ap-southeast-1.rds.amazonaws.com:3306/flight_passenger'
