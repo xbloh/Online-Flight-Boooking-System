a9666d0824d874a725a54013fcf1d91c85848f3d
thao1923 <ptbui.2018@sis.smu.edu.sg>
2020-03-14 15:36:47 +0800

    Update flight_passenger.sql

--- databases/flight_passenger.sql
+++ databases/flight_passenger.sql
@@ -18,9 +18,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-drop database if exists flight_pricing;
-create database flight_pricing;
-use flight_pricing;
+drop database if exists flight_passenger;
+create database flight_passenger;
+use flight_passenger;
 --
 -- Database: `flight_passenger`
 --
