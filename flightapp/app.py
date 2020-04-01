c3bb6d23849bdffd5508b534f0e3dc2c046c0a91
ingsin <46809547+ingsin@users.noreply.github.com>
2020-04-01 14:58:59 +0800

    create booking to cart to paypal

--- flightapp/app.py
+++ flightapp/app.py
@@ -33,5 +33,9 @@ def admin_search_flights():
 def cart():
     return render_template("/cart.html")
 
+@app.route("/paypal")
+def paypal():
+    return render_template("/paypal.html")
+
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
--- flightapp/booking.py
+++ flightapp/booking.py
@@ -222,7 +222,7 @@ def get_booking_by_refCode(refCode):
         url2 = 'http://localhost:5001/flight/' +str(booking.flightNo)
         r2 = requests.get(url2)
         result2 = json.loads(r2.text)
-        return_json = {"pid": booking.pid, "refCode" : booking.refCode, "flightNo": booking.flightNo, "departDate" :booking.departDate, "deptTime": result2['flight']['deptTime'], "class_type": booking.class_type, "seat_number": booking.seat_number }
+        return_json = {"pid": booking.pid, "refCode" : booking.refCode, "flightNo": booking.flightNo, "departDate" :booking.departDate, "deptTime": result2['flight']['deptTime'], "class_type": booking.class_type, "seat_number": booking.seat_number, "price":booking.price }
 
         return {"booking": return_json, "status": 200}
     return jsonify({"message": "Booking not found"}), 404
